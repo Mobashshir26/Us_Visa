a2b43303864e51df50d19f0145364e3e0f7fa3cf
Mobashshir26 <mobashshirali3@gmail.com>
2025-09-25 21:43:38 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Mobashshir",
+    author_email="mobashshirali3@gmail.com",
+    packages=find_packages()
+)
